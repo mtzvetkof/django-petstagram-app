9ece4a1b7840e53def7ff5a2d352885a31fcc652
mtsvetkoff <mtzvetkoff@abv.bg>
2021-09-02 01:29:41 +0300

    Simple application to share photos of your pet/pets. User authentication. Comments and likes are also available per pet photo

--- petstagram/settings.py
+++ petstagram/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'petstagram.common',
     'petstagram.pets',
+    'petstagram.accounts'
 ]
 
 MIDDLEWARE = [
@@ -144,3 +145,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media_files'
+
+AUTH_USER_MODEL = 'accounts.PetstagramUser'
