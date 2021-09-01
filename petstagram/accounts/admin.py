7ca2022ec64cedfb37b116f2c237728f09dba081
mtsvetkoff <mtzvetkoff@abv.bg>
2021-09-02 01:43:23 +0300

    Simple application to share photos of your pet/pets. User authentication. Comments and likes are also available per pet photo

--- petstagram/accounts/admin.py
+++ petstagram/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from petstagram.accounts.models import PetstagramUser
+
+
+@admin.register(PetstagramUser)
+class PetstagramUserAdmin(UserAdmin):
+    list_display = ('email', 'is_staff')
+    list_filter = ('is_staff', 'is_superuser', 'groups')
+    search_fields = ('email',)
+    ordering = ('email',)
